3b9b0ecbd2eae11df0e1395a30c3d3ba349ef6d6
roddg86 <roddg@mail.ru>
2022-08-28 18:49:09 +0900

    Handling touch events

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -1,8 +1,35 @@
 package com.bignerdranch.android.draganddraw
 
 import android.content.Context
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
+private const val TAG = "BoxDrawingView"
+
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val current = PointF(event.x, event.y)
+        var action = ""
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                action = "ACTION_DOWN"
+            }
+            MotionEvent.ACTION_MOVE -> {
+                action = "ACTION_MOVE"
+            }
+            MotionEvent.ACTION_UP -> {
+                action = "ACTION_UP"
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                action = "ACTION_CANCEL"
+            }
+        }
+        Log.i(TAG, "$action at x=${current.x}, y=${current.y}")
+        return true
+    }
+
 }
